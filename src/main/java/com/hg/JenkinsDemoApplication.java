8e64560173b7e01146525bd391954d186d2bd8b2
Lanceff <30307517+lanceff@users.noreply.github.com>
2023-03-01 13:50:37 +0800

    feat(1.0.0):
    初始化

---
+++ src/main/java/com/hg/JenkinsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.hg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 詹金斯演示应用程序
+ *
+ * @author liuhui
+ * @date 2023/03/01
+ */
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/hg/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.hg.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/case1")
+    public String demo1() {
+        return "success";
+    }
+}
