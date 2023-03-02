262468f81777e0f5310c82d9b9941b920ecff91b
liuhui <hliu3@heygears.com>
2023-03-02 13:35:37 +0800

    feat(1.0.0): 初始化

--- README.md
+++
@@ -1,2 +0,0 @@
-# jenkins_demo
-jenkins 案例
--- src/main/java/com/hui/JenkinsDemoApplication.java
+++ src/main/java/com/hui/JenkinsDemoApplication.java
@@ -1,13 +1,11 @@
-package com.hg;
+package com.hui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 詹金斯演示应用程序
- *
  * @author liuhui
- * @date 2023/03/01
+ * @date 2023/03/02
  */
 @SpringBootApplication
 public class JenkinsDemoApplication {
@@ -15,4 +13,5 @@ public class JenkinsDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
+
 }
